6c289031cfcae2177bef1b1c9e1e07ea05e48b75
akolesni <kas1506@mail.ru>
2017-01-12 22:06:25 +0100

    t

---
+++ WpfTest/WpfTest/AltrnatingGridColorSimpleExtension.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTest
+{
+  using System.Reflection;
+  using System.Security.Cryptography.X509Certificates;
+  using System.Windows;
+  using System.Windows.Controls;
+  using System.Windows.Markup;
+  using System.Windows.Media;
+
+  public class AltrnatingGridColorSimpleExtension : MarkupExtension
+  {
+    public Color ColorEven { get; set; }
+    public Color ColorOdd { get; set; }
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+      if (serviceProvider == null) return this;
+
+      IProvideValueTarget service = 
+        serviceProvider.GetService(typeof(IProvideValueTarget))as IProvideValueTarget;
+
+      if (service == null) return this;
+
+      Type targetPropertyType = null;
+      if (service.TargetProperty is DependencyProperty)
+      {
+        targetPropertyType = ((DependencyProperty)service.TargetProperty).PropertyType;
+      }
+      else if (service.TargetProperty is PropertyInfo)
+      {
+        targetPropertyType = ((PropertyInfo)service.TargetProperty).PropertyType;
+      }
+      else
+      {
+        return this;
+      }
+
+      if (!typeof(Brush).IsAssignableFrom(targetPropertyType))
+      {
+        return null;
+      }
+
+      SolidColorBrush brush = new SolidColorBrush(ColorEven);
+
+      if (service.TargetObject is DependencyObject)
+      {
+        var depObject = (DependencyObject)service.TargetObject;
+        var row = (int)depObject.GetValue(Grid.RowProperty);
+        if (row % 2 != 0)
+        {
+          brush.Color = ColorOdd;
+        }
+      }
+
+      return brush;
+    }
+
+    public AltrnatingGridColorSimpleExtension()
+    {
+    }
+  }
+}
